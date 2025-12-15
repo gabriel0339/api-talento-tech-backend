db1283b6fd6d8316c8e480f367d5999f09e5c43c
Gabriel Montoya <129816426+gabriel0339@users.noreply.github.com>
2025-12-15 16:55:07 -0300

    Add comments to index.js for better clarity
    Added comments to clarify middleware and route functionality.

--- api/index.js
+++ api/index.js
@@ -6,18 +6,20 @@ import authRoutes from "../routes/auth.routes.js";
 
 const app = express();
 
-app.use(cors());
-app.use(bodyParser.json());
+app.use(cors()); "Habilita cors para permitir peticiones desde otros dominios"
+app.use(bodyParser.json());"Permite interpretar el cuerpo de las peticiones en formato json"
 
 app.use("/api/products", productsRoutes);
-app.use("/auth", authRoutes);
+app.use("/auth", authRoutes); "Rutas de autenticación ejemplo: /auth/login, /auth/register"
+
+
 
 app.get("/test", (req, res) => {
-  res.json({ message: "API OK" });
+  res.json({ message: "API OK" }); "prueba para verificar que la API está funcionando"
 });
 
 app.use((req, res) => {
-  res.status(404).json({ message: "Ruta no encontrada" });
+  res.status(404).json({ message: "Ruta no encontrada" }); "middleware para manejar rutas no encontradas (404) ,se ejecuta cuando ninguna ruta anterior coincide"
 });
 
 export default app;
