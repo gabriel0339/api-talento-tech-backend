de6c16ae4a0b8985f050300dc03d3b051f7d4bd2
Gabriel Montoya <129816426+gabriel0339@users.noreply.github.com>
2025-12-15 16:56:01 -0300

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -2,10 +2,12 @@ import admin from "firebase-admin";
 
 if (!admin.apps.length) {
   admin.initializeApp({
+    "Configuración de credenciales usando una cuenta de servicio  cargada desde variables de entorno"
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+
     }),
   });
 }
